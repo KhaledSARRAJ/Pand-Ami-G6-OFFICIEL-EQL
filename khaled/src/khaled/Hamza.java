24b1b9169a9385521c7e223fc33dbbbcd34eb2e7
Khaled Sarraj <khaledinat@gmail.com>
2020-12-08 07:37:19 +0100

    nettoyage

--- khaled/src/khaled/Hamza.java
+++
@@ -1,9 +0,0 @@
-package khaled;
-
-public class Hamza {
-
-	public static void main(String[] args) {
-System.out.println("hi");
-	}
-
-}
--- khaled/src/khaled/dexiemetest.java
+++
@@ -1,5 +0,0 @@
-package khaled;
-
-public class dexiemetest {
-
-}
