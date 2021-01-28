38cc37c9afd53d19e067efa1ec39a719569c7d98
mika <mik.sam@hotmail.fr>
2021-01-28 16:01:14 +0100

    Dossier avec la solution test qui fonctionne

--- PANDAMIAPP/MonCatalogueProduits/Controllers/PreferenceJourHoraireController.cs
+++ PANDAMIAPP/MonCatalogueProduits/Controllers/PreferenceJourHoraireController.cs
@@ -1,57 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MonCatalogueProduit.Service;
 using GestionProduits.Service;
+using System.Web.Mvc;
 
 namespace GestionProduits.Controllers
 {
-    public class PreferenceJourHoraireController : Controller
+    public class PreferenceJourHoraireController : System.Web.Mvc.Controller
     {
-        public IActionResult PreferenceJourHoraire()
-        {
-            return View();
-        }
 
-            
+
         //on appel notre catalogue pour avoir accees a la base à l'aide d'une instance
         //à dbcontext et il faut l'ajouter dans les services dans startup
         public CatalogueDbContext dbContext { get; set; }
         //un constructeur pour l'utiliser ensuite
-         public PreferenceJourHoraireController(CatalogueDbContext db)
-            {
-                this.dbContext = db;
-            }
-
-        public IActionResult FormPreferenceJourHoraire()
+        public PreferenceJourHoraireController(CatalogueDbContext db)
         {
-            PreferenceJourHoraire p = new PreferenceJourHoraire();
-            //liste de categorie = IEnumerable 
-            IEnumerable<PreferenceJourHoraire> cats = dbContext.ListePreferenceJourHoraires.ToList();
-            //stocker dans le viewbag la liste de categorie
-            ViewBag.categories = cats;
-            return View("FormPreferenceJourHoraire", p);
+            this.dbContext = db;
         }
 
-        [HttpPost]
-        public IActionResult SavePreferenceJourHoraire(PreferenceJourHoraire p)
+
+
+        public ActionResult Index()
         {
-            IEnumerable<PreferenceJourHoraire> cats = dbContext.ListePreferenceJourHoraires.ToList();
-            ViewBag.categories = cats;
+            return View();
+        }
 
-            if (ModelState.IsValid)
+        public JsonResult GetPreferenceJourHoraire()
+        {
+            using (CatalogueDbContext db = new CatalogueDbContext())
             {
-                dbContext.ListePreferenceJourHoraires.Add(p);
-                dbContext.SaveChanges();
-                return RedirectToAction("Index");
+                var PreferenceJourHoraire = db.ListePreferenceJourHoraires.ToList();
+                return new JsonResult { Data = PreferenceJourHoraire, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
-
-            return View("FormPreferenceJourHoraire", p);
-
         }
 
     }
+
 }
